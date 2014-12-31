33c61f5960fa1cd274ab72d36a0fb65a44f4d555
Sung Won Cho <mikeswcho@gmail.com>
2014-12-31 14:13:15 +1100

    Add fake data for development

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+User.destroy_all
+Oneline.destroy_all
+
+(1..10).each do |n|
+  User.create!(
+    username: "test_user#{n}",
+    email: "test_user#{n}@example.com",
+    password: 'pass1234',
+    password_confirmation: 'pass1234'
+  )
+end
+
+puts "created #{User.count} users."
+
+15.times do
+  Oneline.create!(
+    subject: Faker::Lorem.word,
+    definition: Faker::Lorem.sentence,
+    user_id: (1..10).to_a.sample
+  )
+end
+
+puts "created #{Oneline.count} onelines."
