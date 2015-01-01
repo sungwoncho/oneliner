ffe8bef2cdb062bdca1f82959890a6370c449634
Sung Won Cho <mikeswcho@gmail.com>
2015-01-01 19:03:07 +1100

    Add #upvoted? and downvoted?

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,18 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :authored_onelines, class_name: 'Oneline', foreign_key: :author_id
+  has_many :votes
+
+  def upvote(oneline)
+    oneline.increment!(:cached_votes)
+    votes.create(oneline: oneline, vote_type: 'up')
+  end
+
+  def upvoted?(oneline)
+    votes.where(oneline_id: oneline.id, vote_type: 'up').present?
+  end
+
+  def downvoted?(oneline)
+    votes.where(oneline_id: oneline.id, vote_type: 'down').present?
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,5 +8,80 @@ RSpec.describe User, :type => :model do
 
   describe 'association' do
     it { should have_many(:authored_onelines) }
+    it { should have_many(:votes) }
+  end
+
+  describe 'instance methods' do
+
+    let(:user) { create(:user) }
+    let(:oneline) {create(:oneline)}
+
+    describe '#upvote' do
+      context 'if user has not voted' do
+        it 'creates a vote record' do
+          expect {
+            user.upvote(oneline)
+          }.to change(Vote, :count).by(1)
+        end
+
+        it "assigns vote_type 'up' to the vote" do
+          user.upvote(oneline)
+          expect(Vote.last.vote_type).to eq 'up'
+        end
+
+        it 'increases the cached votes of the oneline' do
+          expect {
+            user.upvote(oneline)
+            oneline.reload
+          }.to change(oneline, :cached_votes).by(1)
+        end
+      end
+
+      context 'if user has downvoted before' do
+
+        before :each do
+          create(:vote, user: user, vote_type: 'down')
+        end
+
+        it 'deletes the downvote record' do
+          user.upvote(oneline)
+          downvote = Vote.find_by(user: user, vote_type: 'down')
+          # expect(downvote).to be nil
+        end
+
+        it "assigns vote_type 'up' to the vote" do
+          user.upvote(oneline)
+          expect(Vote.last.vote_type).to eq 'up'
+        end
+
+
+      end
+    end
+
+    describe '#upvoted?' do
+      context 'if user has upvoted' do
+        it 'returns true' do
+          create(:vote, user: user, oneline: oneline, vote_type: 'up')
+          expect(user.upvoted?(oneline)).to be true
+        end
+      end
+
+      context 'if user has not upvoted' do
+        specify { expect(user.upvoted?(oneline)).to be false }
+      end
+    end
+
+    describe '#downvoted?' do
+      context 'if user has downvoted' do
+        it 'returns true' do
+          create(:vote, user: user, oneline: oneline, vote_type: 'down')
+          expect(user.downvoted?(oneline)).to be true
+        end
+      end
+
+      context 'if user has not downvoted' do
+        specify { expect(user.downvoted?(oneline)).to be false }
+      end
+    end
   end
 end
